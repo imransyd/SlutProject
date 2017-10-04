2c9c4b5ce561165266627c624aaacb94fbd792bf
ceasarantonio <ceasarantonio@users.noreply.github.com>
2017-10-04 09:13:23 +0200

    påbörjat pug

--- grupp3/routes/bokning.js
+++ grupp3/routes/bokning.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('bokning');
 });
 
 module.exports = router;
