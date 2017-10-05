6e0378220af621ff2abd71cf0b9f713d97f8971e
ceasarantonio <ceasarantonio@users.noreply.github.com>
2017-10-05 09:09:13 +0200

    gjort en koppling emellan startsidan och ansökningssidan

--- grupp3/app.js
+++ grupp3/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var bokning = require('./routes/bokning');
+var ansokning = require('./routes/ansokning')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/bokning', bokning);
+app.use('/ansokning', ansokning);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
