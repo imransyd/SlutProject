7a861264d068873c4383674507fd48c94eeeb7c4
Petri Jahnsson <meikyo72@gmail.com>
2017-10-10 10:55:44 +0200

    data to pug RAW

--- grupp3/routes/index.js
+++ grupp3/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb');
 
-/* GET home page. */
+
+MongoClient.connect('mongodb://ECGrupp3:Frontend2016@cluster0-shard-00-00-dmlri.mongodb.net:27017,cluster0-shard-00-01-dmlri.mongodb.net:27017,cluster0-shard-00-02-dmlri.mongodb.net:27017/fordondb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', function(err, db){
+    if (err) throw err
+    
+    db.collection('fordon').find().toArray(function (err, result){
+        if (err) throw err
+        
+        console.log('Connected to database');
+        
+        var fordoncollection = db.collection('fordon');
+        
+        
+
+
+/* GET home page.*/ 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    fordoncollection.find({}).toArray(function(err, fordonResult) {
+            if (err) {
+                res.send(err);
+            } else if (fordonResult.length) {
+                res.render('index', {
+                    'fordonlist': fordonResult,
+                });
+            } else {
+                res.send('No documents found');
+            }
+            db.close();
+    })})})
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
