cccef7d426dfefc74ea50cf72e099e8b0857f0fe
ceasarantonio <ceasarantonio@users.noreply.github.com>
2017-10-13 09:58:45 +0200

    last fix

--- grupp3/app.js
+++ grupp3/app.js
@@ -33,18 +33,15 @@ app.use('/kvitto', kvitto);
 app.use('/nybokning', nybokning);
 
 app.post('/process_post', function(req, res){
-  console.log("hej" + req.body.fordonstyp)
   res.render('ansokning', {fordonstyp: req.body.fordonstyp, korkort: req.body.korkort, marke: req.body.marke, vaxellada: req.body.vaxellada, modell: req.body.modell, arsmodell: req.body.arsmodell, dagshyra: req.body.dagshyra, id: req.body.id})
   
 })
 
 app.post('/kvitto', function(req, res){
-  console.log("hallo" + req.body.id)
     
     MongoClient.connect('mongodb://ECGrupp3:Frontend2016@cluster0-shard-00-00-dmlri.mongodb.net:27017,cluster0-shard-00-01-dmlri.mongodb.net:27017,cluster0-shard-00-02-dmlri.mongodb.net:27017/fordondb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', function(err, db){
         if (err) throw err
         var ObjId = "ObjectId"+'("'+req.body.id+'")';
-        console.log(ObjId);
     try {
         db.collection('fordon').update({"_id": ObjectId(req.body.id)},
                             {
